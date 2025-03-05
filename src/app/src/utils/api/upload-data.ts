e8da2820e1e380cbecc83aed693c2ace6c3a5f44
artem <artemka77752@gmail.com>
2025-03-05 13:32:24 +0300

    user registration updated

--- src/app/src/utils/api/upload-data.ts
+++ src/app/src/utils/api/upload-data.ts
@@ -1,32 +1,55 @@
 import axios from "axios";
+import { AxiosError } from "axios";
+
+import { initDataUser } from "@telegram-apps/sdk-react";
+import { RegistrationUserForm } from "../../types/client-interfaces";
 import { UserResponse } from "../../types/data-interfaces";
 import { ENDPOINTS } from "../endpoints";
-import { RegistrationUserForm } from "../../types/client-interfaces";
 
 export const uploadData = async (
   fromData: RegistrationUserForm,
   images: File[]
-) => {
+): Promise<UserResponse> => {
+  const user = initDataUser();
+  if (!user) {
+    throw new Error("Пользователь не определён");
+  }
+
   const formData = new FormData();
 
-  // Добавляем данные формы
+  formData.append("id", user.id.toString());
+  formData.append("first_name", user.firstName || "");
+  formData.append("last_name", user.lastName || "");
+  formData.append("username", user.username || "");
+  formData.append("language_code", user.languageCode || "");
+
   Object.entries(fromData).forEach(([key, value]) => {
     formData.append(key, value.toString());
   });
 
-  // Добавляем изображения
-  images.forEach((image, index) => {
+  images.forEach((image) => {
     if (image) {
-      formData.append(`image${index + 1}`, image);
+      formData.append("images", image);
     }
   });
+
+  console.log("Данные формы:", Array.from(formData.entries()));
+
   try {
-    const response = await axios.post<UserResponse>(ENDPOINTS.BACKEND.USER, 
-        formData
+    const response = await axios.post<UserResponse>(
+      ENDPOINTS.BACKEND.USER,
+      formData
     );
-    return response.data
+    return response.data;
   } catch (error) {
-    console.error(`Ошибка при регистрации ${error}`)
-    throw error
+    if (error instanceof AxiosError) {
+      console.error(
+        "Ошибка при регистрации:",
+        (error as AxiosError).response?.data || (error as AxiosError).message
+      );
+    } else {
+      console.error("Неизвестная ошибка:", error);
+    }
+    throw error;
   }
 };
